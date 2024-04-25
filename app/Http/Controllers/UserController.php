5518e4d12fafb934cbd460e381a7839f8c5a8b73
aekkakti <music.tomsk1@gmail.com>
2024-04-25 12:56:19 +0700

    Сделана валидация для регистрации пользователя

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
 use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
@@ -14,16 +15,15 @@ class UserController extends Controller
     public function registration(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'email' => ['required', 'unique:users,email'],
-            'password' => ['required'],
-            'first_name' => ['required'],
+            'email' => ['required', 'email', 'unique:users,email'],
+            'password' => ['required', Password::min(3)->mixedCase()->numbers()],
+            'first_name' => ['required', 'min:2'],
             'last_name' => ['required']
         ]);
 
         if ($validator->fails()) {
             return response()->json(['success' => false, 'code' => 422, 'message' => $validator->errors()], 422);
         }
-
 
 
         $user = User::create([
